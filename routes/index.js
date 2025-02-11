d7f5ad3c02e3c2a44f6ada27586e8284ea25bbb8
NoahClemmensen <noah0940@edu.sde.dk>
2025-02-11 12:35:46 +0100

    new endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const Database = require('../services/database');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/hello', function(req, res, next) {
+  res.json({ message: 'Hi'})
+});
+
+router.get('/getPendingPackages', async function(req, res, next) {
+  const pendingPackages = await Database.getPendingPackages();
+
+  res.json(pendingPackages);
+});
+
+router.post('/registerPackages', async function(req, res, next) {
+  const packages = req.body;
+
+  let success = true;
+  for (let i = 0; i < packages.length; i++) {
+    const result = await Database.markPackageDelivered(packages[i]);
+    if (result.affectedRows === 0) success = false;
+  }
+
+  res.json(success);
 });
 
 module.exports = router;
---
+++ services/database.js
@@ -0,0 +1,44 @@
+const mysql = require('mysql2/promise');
+
+console.log(process.env.DB_HOST);
+
+let userConf = {
+    socketPath: process.env.DB_SOCKET,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+}
+
+let userConn = mysql.createConnection(userConf);
+
+class Database {
+    static async getPendingPackages() {
+        return this.query('SELECT * FROM pending_packages');
+    }
+
+    static async markPackageDelivered(packageId) {
+        return this.queryProcedure('delivery_status_delivered(?)', [packageId]);
+    }
+
+    static async query(sql, args) {
+        if (userConn instanceof Promise) {
+            userConn = await userConn;
+        }
+
+        const [results, fields] = await userConn.query(sql, args);
+        return results;
+    }
+
+    static async queryProcedure(sql, args) {
+        if (userConn instanceof Promise) {
+            userConn = await userConn;
+        }
+
+        const [results, fields] = await userConn.query('call ' + sql, args);
+        return results;
+    }
+}
+
+module.exports = Database;
